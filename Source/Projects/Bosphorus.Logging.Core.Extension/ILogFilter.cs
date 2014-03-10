93a6a09a76e8c2dc34a0ae3a40a01669e09b28de
Onur EKER <onur.eker@gmail.com>
2014-03-10 20:54:26 +0200

    * Facade project is removed from solution * Unnecessary project Bosphorus.Logging.Core.Extension is deleted

--- Source/Projects/Bosphorus.Logging.Core.Extension/ILogFilter.cs
+++
@@ -1,7 +0,0 @@
-namespace Bosphorus.Library.Logging.Core.Extension
-{
-    public interface ILogFilter
-    {
-        bool IsLoggable<TLogModel>(TLogModel logModel) where TLogModel : ILogModel;
-    }
-}

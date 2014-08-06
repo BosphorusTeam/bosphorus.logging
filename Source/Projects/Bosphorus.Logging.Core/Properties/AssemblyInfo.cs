18fedc7f90be6b51804b00f65a10e24a4d08a3df
Onur EKER <onur.eker@gmail.com>
2014-08-06 10:11:26 +0300

    * NuGet update is applied * NuSpec is added

--- Source/Projects/Bosphorus.Logging.Core/Properties/AssemblyInfo.cs
+++ Source/Projects/Bosphorus.Logging.Core/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bosphorus.Library.Logging.Core")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bosphorus.Library.Logging.Core")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bosphorus")]
 [assembly: AssemblyProduct("Bosphorus.Library.Logging.Core")]
 [assembly: AssemblyCopyright("Copyright ©  2009")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Bosphorus")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
--- Source/Projects/Bosphorus.Logging.Database/Properties/AssemblyInfo.cs
+++ Source/Projects/Bosphorus.Logging.Database/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Bosphorus.Logging.Database")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Bosphorus.Logging.Database")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bosphorus")]
 [assembly: AssemblyProduct("Bosphorus.Logging.Database")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Bosphorus")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 

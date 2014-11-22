5b8740367af97579b106ba33c02329ddfb5e25cb
Onur EKER <onur.eker@gmail.com>
2014-11-22 11:58:45 +0200

    AssemblyInfo.cs is update

--- Source/Demos/Bosphorus.Logging.Facade.Demo/Properties/AssemblyInfo.cs
+++ Source/Demos/Bosphorus.Logging.Facade.Demo/Properties/AssemblyInfo.cs
@@ -29,8 +29,10 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.0.0.4")]
-[assembly: AssemblyFileVersion("1.0.0.4")]
+[assembly: AssemblyVersion("1.0.0.5")]
+[assembly: AssemblyFileVersion("1.0.0.5")]
+
+
 
 
 

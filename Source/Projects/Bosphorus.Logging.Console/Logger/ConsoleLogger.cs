16ca19a1b179bc1e8e9e751544f1e26104fa1241
Onur EKER <onur.eker@gmail.com>
2015-10-22 18:43:14 +0300

    Re-factor

--- Source/Projects/Bosphorus.Logging.Console/Logger/ConsoleLogger.cs
+++ Source/Projects/Bosphorus.Logging.Console/Logger/ConsoleLogger.cs
@@ -8,18 +8,18 @@ namespace Bosphorus.Logging.Console.Logger
     public class ConsoleLogger<TLog> : AbstractLogger<TLog>, IConsoleLogger<TLog>, ILogger<TLog>
         where TLog : ILog
     {
-        private static readonly IDictionary<LogLevel, ConsoleColor> logColors;
         private readonly string logFormat;
+        private static IDictionary<LogLevel, ConsoleColor> LogColors { get; }
 
         static ConsoleLogger()
         {
-            logColors = new Dictionary<LogLevel, ConsoleColor>();
-            logColors.Add(LogLevel.Debug, ConsoleColor.Cyan);
-            logColors.Add(LogLevel.Error, ConsoleColor.Red);
-            logColors.Add(LogLevel.Fatal, ConsoleColor.Red);
-            logColors.Add(LogLevel.Info, ConsoleColor.Green);
-            logColors.Add(LogLevel.Null, ConsoleColor.White);
-            logColors.Add(LogLevel.Warn, ConsoleColor.Yellow);
+            LogColors = new Dictionary<LogLevel, ConsoleColor>();
+            LogColors.Add(LogLevel.Debug, ConsoleColor.Cyan);
+            LogColors.Add(LogLevel.Error, ConsoleColor.Red);
+            LogColors.Add(LogLevel.Fatal, ConsoleColor.Red);
+            LogColors.Add(LogLevel.Info, ConsoleColor.Green);
+            LogColors.Add(LogLevel.Null, ConsoleColor.White);
+            LogColors.Add(LogLevel.Warn, ConsoleColor.Yellow);
         }
 
         public ConsoleLogger(Configuration<TLog> configuration)
@@ -30,7 +30,7 @@ namespace Bosphorus.Logging.Console.Logger
         public override void Log(TLog log)
         {
             //TODO: Not thread safe, color are conflicted and race each other :))
-            ConsoleColor consoleColor = logColors[log.Level];
+            ConsoleColor consoleColor = LogColors[log.Level];
             System.Console.ForegroundColor = consoleColor;
             System.Console.WriteLine(logFormat, log.DateTime, log.Level, log.Message);
             System.Console.ResetColor();
